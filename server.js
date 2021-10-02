35ab802ecadfe50ab58a14c031cd0df605b6292d
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-02 13:00:24 -0500

    server.js updated: express middleware added

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express').Router();
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
