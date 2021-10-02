22282ad09abb58ddcc4aa2db3bc79d8f61401a66
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-02 13:19:47 -0500

    routes/html.js updated: get route for public/stats.html created

--- routes/html.js
+++ routes/html.js
@@ -8,3 +8,7 @@ router.get('/', (req, res) => {
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname + '../public/exercise.html'));
 });
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname + '../public/stats.html'));
+});
