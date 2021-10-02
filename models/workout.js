6d3dc45a7b28333fd3b9281c9f2059ca9371f8da
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-02 13:07:42 -0500

    models/workout.js updated: schema created

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,43 @@
 const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [{
+
+        type: {
+            type: String,
+            trim: true,
+        },
+
+        name: {
+            type: String,
+            trim: true
+        },
+
+        duration: {
+            type: Number
+        },
+
+        weight: {
+            type: Number
+        },
+
+        reps: {
+            type: Number
+        },
+
+        sets: {
+            type: Number
+        },
+
+        distance: {
+            type: Number
+        }
+    }]
+});
