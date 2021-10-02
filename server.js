fb76129320ff256095540773fda2013a6dd37fda
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-02 13:02:19 -0500

    server.js updated: function to run server on specified port created

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts', {
 
 app.use(require('./routes/api'));
 app.use(require('./routes/html'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
