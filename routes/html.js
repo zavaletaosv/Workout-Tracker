fa115c149f2f2f907e4d429e3e7f8f2a8ddfe850
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-02 13:19:27 -0500

    routes/html.js updated: get route for public/exercise.html created

--- routes/html.js
+++ routes/html.js
@@ -4,3 +4,7 @@ const path = require('path');
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '../public/index.html'));
 });
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname + '../public/exercise.html'));
+});
